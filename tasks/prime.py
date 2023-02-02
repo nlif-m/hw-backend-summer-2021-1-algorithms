7497424c04eb0109b9f2ca48ac4b9698a144853a
nlif <m.payunen@gmail.com>
2023-02-02 13:00:32 +0300

    implement 'is_prime' function

--- tasks/prime.py
+++ tasks/prime.py
@@ -7,4 +7,10 @@ def is_prime(number: int) -> bool:
     """
     Функция должна вернуть True если число является простым, иначе - False
     """
-    raise NotImplementedError
+    if number < 2:
+        return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+
+    return True
